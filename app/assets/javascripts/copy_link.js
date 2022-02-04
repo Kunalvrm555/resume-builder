a7648e8793abbacc584d43cd10a6f49e204fdd68
Kunal Verma <67605729+Kunalvrm555@users.noreply.github.com>
2022-02-04 16:54:24 +0530

    copy link for preview refined

--- app/assets/javascripts/copy_link.js
+++ app/assets/javascripts/copy_link.js
@@ -2,16 +2,21 @@
 //= require jquery
 //= require jquery_ujs
 // alert("HEY");
-$(document).ready(function(){  
-  
-    var clipboard = new Clipboard('.clipboard-btn');
-   console.log(clipboard);
-  });
+$(document).ready(function () {
+
+  var clipboard = new Clipboard('.clipboard-btn');
+  console.log(clipboard);
+
+  var elem = document.getElementById("btn1");
+  elem.addEventListener("click", function () {
+    elem.innerHTML = "Copied ";
+  });;
+});
 
 // var $main = $('main');
 
 // if (id = $main.data('profile-id'))
-//     return $main; 
+//     return $main;
 //     $(document).on('click', '.', function() {
 //         var text;
 //         if (text = this.getAttribute('data-clipboard-text')) {
