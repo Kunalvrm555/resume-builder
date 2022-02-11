219415dc8862591470f5a97986518ccdec6d38e5
Kunal Verma <67605729+Kunalvrm555@users.noreply.github.com>
2022-02-11 19:09:35 +0530

    add projects to experiences sec

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,27 +4,8 @@ class ProjectsController < ApplicationController
     def new
         @experience = Experience.find(params[:experience_id])
         @experience.projects.create
-        # @experience.projects.create
-        # @project.experience_id = params[:id]
-        # @project = Project.new(project_params)
         flash[:success] = "Project added."
         redirect_to edit_url
     end
-   
-    # def get_exp
-    #     @experience = Experience.find_by(id: params[:id])
-    # end
-    # def update
-    #     @experience = Experience.find(@project.experience_id) 
-    #     @experience.projects.create
-    #     project[:experience_id] = @experience.id
-    #     flash[:success] = "Project added."
-    #     redirect_to edit_url
-    # end
-    # private
-    # def project_params
-    #     params.require(:project).permit(:title,:url, :stack, :description)
-        
-    # end
 
 end
